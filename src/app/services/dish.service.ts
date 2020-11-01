5a9e6f747aa2e305ad5be6a4f4421c34c23fb7aa
Moshe Piamenta <moshep@moveo.co.il>
2020-11-01 19:07:35 +0200

    get popular rests and get signature dishes

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { environment } from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class DishService {
 
-  constructor() { }
+  apiUrl: string;
+
+  constructor(private http: HttpClient) {
+    this.apiUrl = environment.apiUrl+'/dishes'
+   }
+
+   async getSignatureDishes(restsArr: string[]) {
+    try {
+      const sigDishes = await this.http.post(`${this.apiUrl}/signature`, restsArr).toPromise();
+      return sigDishes;
+    } catch(error) {
+      return error.message;
+    }
+   }
 }
--- src/app/services/restaurant.service.ts
+++ src/app/services/restaurant.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { environment } from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class RestaurantService {
 
-  constructor() { }
+  apiUrl: string;
+
+  constructor(private http: HttpClient) {
+    this.apiUrl = environment.apiUrl+'/restaurants'
+   }
+
+  async getPopular(numOfRests: string) {
+    try {
+      const popularRests = await this.http.get(`${this.apiUrl}/popular`,{params: {number: numOfRests}}).toPromise();
+      return popularRests;
+    } catch(error) {
+      return error.message;
+    }
+  }
 }
