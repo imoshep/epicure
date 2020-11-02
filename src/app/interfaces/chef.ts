4dbdc1f51dbbd06ee9fcda66c011da9527e5b2c6
Moshe Piamenta <moshep@moveo.co.il>
2020-11-02 14:07:57 +0200

    fixed interfaces

--- src/app/interfaces/chef.ts
+++ src/app/interfaces/chef.ts
@@ -1,27 +1,14 @@
-import { CardData } from './card-data';
-import { RestaurantDocument } from './restaurant';
-
 export interface Chef {
     fullName: string;
     imageUrl: string;
     desc: string;
-    restaurants: CardData[];
   }
 
 export interface ChefFromDB {
   name: string;
   about?: string;
   imageUrl?: string;
-  // restaurants?: string[] | RestaurantDocument[];
 }
 
 
-interface ChefBaseDocument extends  ChefFromDB, Document {}
-
-// export interface ChefDocument extends ChefBaseDocument {
-//   restaurants: string[];
-// }
-
-// export interface ChefPopulatedDocument extends ChefBaseDocument {
-//   restaurants:  RestaurantDocument[];
-// }
+export interface ChefDocument extends  ChefFromDB, Document {}
